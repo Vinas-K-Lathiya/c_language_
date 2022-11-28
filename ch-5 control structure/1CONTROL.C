4555c6427fea593d2ac74b00f2fdbdddc9b2dc14
Vinas-K-Lathiya <118763065+Vinas-K-Lathiya@users.noreply.github.com>
2022-11-28 10:54:55 +0530

    control structure

---
+++ ch-5 control structure/1CONTROL.C
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+main()
+{
+	int a,b,c;
+	clrscr();
+	printf("enter value of a:");
+	scanf("%d",&a);
+	printf("enter value of b:");
+	scanf("%d",&b);
+	printf("enter value of c:");
+	scanf("%d",&c);
+
+	if(a<b)
+	{
+		if(a<c)
+		{
+			printf("a is small");
+		}
+		else
+		{
+			printf("b is small");
+		}
+	}
+	else
+	{
+		if(b<c)
+		{
+			printf("b is small");
+		}
+		else
+		{
+			printf("c is small");
+		}
+	}
+	getch();
+
+}
