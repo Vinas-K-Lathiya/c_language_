46cb33d8a4b1f45793a0675b3c4cc67a26d3eeda
Vinas-K-Lathiya <118763065+Vinas-K-Lathiya@users.noreply.github.com>
2022-12-09 11:01:47 +0530

    pattern 7.1

---
+++ pattern 7.1/16PATTER.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int i,j;
+	clrscr();
+	for(i=5; i>=1;i--)
+	{
+		for(j=1; j<=i; j++){
+			if((i+j)%2==0){
+			printf("1");
+			}
+			else{
+			printf("0");
+			}
+		}
+		printf("\n");
+
+	}
+
+	getch();
+}
